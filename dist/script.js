1cf352393109d4ed7ecdd16b5206404cabd27b37
Muhammad Raihan Agust Nugroho <agustraihan@gmail.com>
2023-07-31 22:57:39 +0700

    portofolio

---
+++ dist/script.js
@@ -0,0 +1,21 @@
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const navfix = header.offsetTop;
+
+    if (window.pageYOffset > navfix) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+// hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navbar = document.querySelector('#navbar');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navbar.classList.toggle('hidden')
+});
