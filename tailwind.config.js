2a701c1e1b383bdf370052b09fb8bb0bfcc55510
Muhammad Raihan Agust Nugroho <agustraihan@gmail.com>
2023-08-02 21:08:09 +0700

    adding github link

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    screens: {
+      'sm': '850px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1100px',
+      // => @media (min-width: 1100px) { ... }
+      'xl': '1200px',
+    },
     extend: {},
   },
   plugins: [],
