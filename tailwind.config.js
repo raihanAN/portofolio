15098d490f1fcdf4b599ee6057c5d0774aa628eb
Muhammad Raihan Agust Nugroho <agustraihan@gmail.com>
2023-08-06 20:56:40 +0700

    ADDING PROJECT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "Project.html"],
   theme: {
     screens: {
       'sm': '850px',
